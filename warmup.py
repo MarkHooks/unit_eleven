695ff7e0fa9ca09d527c8c865aa32536eaf9a1bb
Mark <mark@iMac2.local>
2018-12-14 14:37:50 -0500

    added another block

--- warmup.py
+++ warmup.py
@@ -12,6 +12,7 @@ main_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 32, 0)
 pygame.display.set_caption("Animation")
 
 BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 WIDTH = 25
 HEIGHT = 25
@@ -22,21 +23,25 @@ my_block.rect.y = 10
 main_window.blit(my_block.image, my_block.rect)
 pygame.display.update()
 
+my_block = block.Block(main_window, WIDTH, HEIGHT, BLUE)
+new_block = block.Block(main_window, WIDTH, HEIGHT, GREEN)
+
+my_block.rect.x = 10
+my_block.rect.y = 10
+new_block.rect.x = WINDOW_WIDTH - 35
+new_block.rect.y = WINDOW_WIDTH - 35
 
 while True:
     for event in pygame.event.get():
         if event == QUIT:
             pygame.quit()
             sys.exit()
-    if my_block.rect.top < 0 or my_block.rect.bottom > 500:
-        speedy = -speedy
-    if my_block.rect.left < 0 or my_block.rect.right > 500:
-        speedx = - speedx
-
     main_window.fill(WHITE)
-    my_block.rect.left += speedx
-    my_block.rect.top += speedy
+    my_block.move()
+    new_block.move()
+    main_window.blit(new_block.image, new_block.rect)
     main_window.blit(my_block.image, my_block.rect)
+
     pygame.display.update()
 
 
